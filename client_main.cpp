41ead4f43cd453a76fb78839672ea5fe41ca6b5a
cordeirolibel <gustavolibel@gmail.com>
2017-11-03 15:50:13 -0200

    send, receive, bin e ami

--- client_main.cpp
+++ client_main.cpp
@@ -1,5 +1,4 @@
 #include <FL/Fl.H>
-#include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
@@ -15,22 +14,81 @@
 
 using namespace std;
 
+Fl_Window *window ;
+
 ClientSocket *client_socket;
+
 Fl_Input *send_input;
+Fl_Box *bin_send_box;
+Fl_Box *ami_send_box;
+
+Fl_Box *bin_receive_box;
+Fl_Box *ami_receive_box;
+Fl_Box *receive_box;
+
+string ascii_to_bin(string str){
+	return ("bin:"+str);
+}
+string bin_to_ami(string str){
+	return ("ami:"+str);
+}
+string ami_to_bin(string str){
+	return (str.substr(4));
+}
+string bin_to_ascii(string str){
+	return (str.substr(4));
+}
 
 void send_button_pushed(Fl_Widget*, long int) {
 
-	string reply;
+	string data = send_input->value();
+
+	string bin = ascii_to_bin(data);
+	string ami = bin_to_ami(bin);
 
 	try	{
-		*client_socket << send_input->value();
-		*client_socket >> reply;
+		*client_socket << ami;
+		*client_socket >> ami;
 	}
 	catch ( SocketException& ) {}
 
-	cout << "We received this response from the server:\n\"" << reply << "\"\n";
+	bin = "Binario: "+bin;
+	ami = "AMI: "+ami;
+	bin_send_box->copy_label(bin.c_str());
+	ami_send_box->copy_label(ami.c_str());
+
+	cout << "Send: " << data << endl;
+	cout << "  " << bin << endl;
+	cout << "  " << ami << endl;
+
+	window->redraw();
 }
 
+void receive_button_pushed(Fl_Widget*, long int) {
+	
+	string data;
+
+	try	{
+		*client_socket >> data;
+		*client_socket << data;
+	}
+	catch ( SocketException& ) {}
+
+	string bin = ami_to_bin(data);
+	string ascii = bin_to_ascii(bin);
+
+	data = "AMI: "+data;
+	bin = "Binario: "+bin;
+	bin_receive_box->copy_label(bin.c_str());
+	ami_receive_box->copy_label(data.c_str());
+	receive_box->copy_label(ascii.c_str());
+
+	cout << "Receive: " << ascii << endl;
+	cout << "  " << bin << endl;
+	cout << "  " << data << endl;
+
+	window->redraw();
+}
 
 int main ()
 {
@@ -49,11 +107,11 @@ int main ()
 	///======================================================
 
 	// Criando janela base
-	int w_max = Fl::w()/2;
-	int h_max = Fl::h()/2;
+	int w_max = Fl::w()/2.5;
+	int h_max = Fl::h()/2.5;
 	int w{w_max}, h{h_max};
 	string title{"Client Socket"};
-	Fl_Double_Window *window = new Fl_Double_Window(w, 0, w, h, title.c_str());
+	window = new Fl_Window(w, 0, w, h, title.c_str());
 
 	// Titulo no topo
 	char str1[] = "Client Socket: Comunicacao de Dados (UTFPR)";
@@ -67,10 +125,26 @@ int main ()
 	box2->labelsize(10);
 
 	// Send Line
-	send_input = new Fl_Input (w_max*0.1 , h_max*0.2, w_max*0.4, h_max*0.08, "Data:");
-	Fl_Button *send_button = new Fl_Button(w_max*0.55, h_max*0.2, w_max*0.2, h_max*0.08, "Send");
+	send_input = new Fl_Input (w_max*0.15, h_max*0.15, w_max*0.4, h_max*0.08, "Data:");
+	Fl_Button *send_button = new Fl_Button(w_max*0.6, h_max*0.15, w_max*0.2, h_max*0.08, "Send");
 	send_button->callback(send_button_pushed);
 
+	// Binario e AMI send lines
+	bin_send_box = new Fl_Box(w_max*0.05,h_max*0.35,w_max*0.9,h_max*0.05,"Binario: ");
+	bin_send_box->align (FL_ALIGN_TOP_LEFT );
+	ami_send_box = new Fl_Box(w_max*0.05,h_max*0.45,w_max*0.9,h_max*0.05,"AMI: ");
+	ami_send_box->align (FL_ALIGN_TOP_LEFT );
+
+	// Receive Line
+	receive_box = new Fl_Box (w_max*0.15, h_max*0.55, w_max*0.4, h_max*0.08, "--");
+	Fl_Button *receive_button = new Fl_Button(w_max*0.6, h_max*0.55, w_max*0.2, h_max*0.08, "Receive");
+	receive_button->callback(receive_button_pushed);
+
+	// Binario e AMI receive lines
+	bin_receive_box = new Fl_Box(w_max*0.05,h_max*0.75,w_max*0.9,h_max*0.05,"Binario: ");
+	bin_receive_box->align (FL_ALIGN_TOP_LEFT );
+	ami_receive_box = new Fl_Box(w_max*0.05,h_max*0.85,w_max*0.9,h_max*0.05,"AMI: ");
+	ami_receive_box->align (FL_ALIGN_TOP_LEFT );
 
 	///======================================================
 	/// ===== Loop
@@ -79,8 +153,7 @@ int main ()
 
 	int k = 1;
 	while (true){
-		cout << k << ' ' << Fl::wait(0.1) << endl;
-		k++;
+		Fl::wait(1) ;
 	}
 
 
